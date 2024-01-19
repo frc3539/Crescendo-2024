a539edbed6ffb06e078d30c723d9e6d03bd82afd
BytingBulldogs3539 <bbprogrammers3539@gmail.com>
2024-01-18 20:31:58 -0500

    Create Shooter Motor

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -4,16 +4,31 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.IDConstants;
 
 public class ShooterSubsystem extends SubsystemBase {
   /** Creates a new ShooterSubsystem. */
   private TalonFX topMotor, bottomMotor, feedMotor;
   
-  public ShooterSubsystem() {}
+  public ShooterSubsystem() {
+    topMotor=new TalonFX(IDConstants.topMotor, "canivore");
+    bottomMotor=new TalonFX(IDConstants.bottomMotor,"canivore");
+    feedMotor=new TalonFX(IDConstants.feedMotor,"canivore");
+  }
 
+  public void setTopMotorSpeed(double rps){
+    topMotor.setControl(new VelocityVoltage(rps).withEnableFOC(true));
+  }
+ public void setBottomMotorSpeed(double rps){
+    bottomMotor.setControl(new VelocityVoltage(rps).withEnableFOC(true));
+  }
+   public void setFeedMotorSpeed(double rps){
+    feedMotor.setControl(new VelocityVoltage(rps).withEnableFOC(true));
+  }
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
