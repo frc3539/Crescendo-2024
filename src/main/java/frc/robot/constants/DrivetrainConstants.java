239e99f84dab1a0488e1645b55dfeb66d4f7181b
BytingBulldogs3539 <bbprogrammers3539@gmail.com>
2024-01-21 20:18:58 -0500

    Add auton drivetrain constants

--- src/main/java/frc/robot/constants/DrivetrainConstants.java
+++ src/main/java/frc/robot/constants/DrivetrainConstants.java
@@ -7,4 +7,17 @@ public class DrivetrainConstants extends BBConstants{
 		super("/home/lvuser/DrivetrainConstants.ini", true);
 		save();
 	}
+	public static double TranslationkP = 20.0;
+	public static double TranslationkI = 0.0;
+	public static double TranslationkD = 0.0;
+	public static double RotationkP = 10.0;
+	public static double RotationkI = 0.0;
+	public static double RotationkD = 0.0;
+	public static double TranslationkV = 1.0;
+	public static double TranslationkA = 0.0;
+	public static double TranslationkS = 0.0;
+	public static double FLSteerOffset = 0.0;
+	public static double FRSteerOffset = 0.0;
+	public static double BLSteerOffset = 0.0;
+	public static double BRSteerOffset = 0.0;
 }
