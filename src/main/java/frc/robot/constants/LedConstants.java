899f2b802629fe694fe2336196333fe83e25ceb0
Programmers3539 <bbprogrammers3539@gmail.com>
2024-01-22 20:02:34 -0500

    Made color constants

--- src/main/java/frc/robot/constants/LedConstants.java
+++ src/main/java/frc/robot/constants/LedConstants.java
@@ -4,5 +4,15 @@
 
 package frc.robot.constants;
 
+import java.awt.Color;
+
 /** Add your docs here. */
-public class LedConstants {}
+public class LedConstants {
+
+public static int maxBrightness = 0;
+public static int flashSpeed = 0;
+public static int numLights = 0;
+public static Color Green = new Color(0, 255, 0);
+public static Color Orange = new Color(255, 100, 0);
+public static Color Blue = new Color(0, 0,255);
+}
