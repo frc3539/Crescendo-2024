88968a0ec8d49d1537781319f9393ac9f40d831d
Programmers3539 <bbprogrammers3539@gmail.com>
2024-02-03 09:22:20 -0500

    Shooter Constants Saved

--- src/main/java/frc/robot/constants/ShooterConstants.java
+++ src/main/java/frc/robot/constants/ShooterConstants.java
@@ -3,18 +3,18 @@ package frc.robot.constants;
 import org.frcteam3539.BulldogLibrary.INIConfiguration.BBConstants;
 
 public class ShooterConstants extends BBConstants {
-public ShooterConstants() {
-	super("/home/lvuser/ShooterConstants.ini", true);
-	save();
-}
+	public ShooterConstants() {
+		super("/home/lvuser/ShooterConstants.ini", true);
+		save();
+	}
 
-public static double revRps = 0.0;
-public static double shootRps = 0.0;
-public static double angleShooterP = 0.0;
-public static double angleShooterI = 0.0;
-public static double angleShooterD = 0.0;
-public static double angleShooterV = 0.0;
-public static double angleShooterG = 0.0;
-public static int angleShooterSoftMin = 0;
-public static int angleShooterSoftMax = 0;
+	public static double revRps = 0.0;
+	public static double shootRps = 0.0;
+	public static double angleShooterP = 0.0;
+	public static double angleShooterI = 0.0;
+	public static double angleShooterD = 0.0;
+	public static double angleShooterV = 0.0;
+	public static double angleShooterG = 0.0;
+	public static int angleShooterSoftMin = 0;
+	public static int angleShooterSoftMax = 0;
 }
